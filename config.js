164e4f923b2e97181a41e4512288ab77bcb84814
FXorange-CPU <132431774+FXorange-CPU@users.noreply.github.com>
2025-04-23 21:59:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 window.IMG_BED_CONFIG = {
-  apiBaseUrl: "https://image.liuju8956.workers.dev",  // Worker API 地址
+  apiBaseUrl: "https://*-image.liuju8956.workers.dev",  // Worker API 地址
   maxFiles: 3                          // 最大上传文件数
 };
